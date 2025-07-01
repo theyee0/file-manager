4278e771a6d5fafa992daed42b56cc08b2573239
Jim Chen <theyee0@gmail.com>
2025-07-01 11:57:20 -0700

    Added basic layout for future file manager, with colors for testing

--- src/file_manager/interface.clj
+++ src/file_manager/interface.clj
@@ -1,19 +1,39 @@
 (ns file-manager.interface
   (:gen-class)
-  (:import [java.awt Color GridBagLayout GridBagConstraints Insets])
-  (:import [javax.swing JLabel JButton JPanel JFrame]))
+  (:import [java.awt
+            Color GridBagLayout BorderLayout GridBagConstraints
+            Insets Dimension])
+  (:import [javax.swing JLabel JButton JPanel JFrame JTree JScrollPane]))
 
-(defmacro set-grid! [constraint field value]
+
+;; Simplifies the use of GridBagLayout
+
+(defmacro set-grid!
+  "Given a constraint, change a parameter to a specified value"
+  [constraint field value]
   `(set! (. ~constraint ~(symbol (name field)))
          ~(if (keyword? value)
             `(. java.awt.GridBagConstraints
                 ~(symbol (name value)))
             value)))
 
-(defmacro grid-bag-layout [container & body]
+
+(defmacro grid-bag-layout
+  "Given a GridBagLayout, add items in the body with specified parameters and objects:
+  i.e.
+  (grid-bag-layout
+    GridBagLayout-Variable
+    :weight-1 :value-1 :weight-2 :value-2
+    (Java-Swing-Object-1)
+    :weight-3 :value-3 :weight-4 :value-4
+    (Java-Swing-Object-2)
+  ...                    )
+
+  Outputs a spliced list of (set-grid!) calls"
+  [container & body]
   (let [c (gensym "c")
         cntr (gensym "cntr")]
-    `(let [~c (new java.awt.GridBagConstraints)
+    `(let [~c (GridBagConstraints.)
            ~cntr ~container]
        ~@(loop [result '() body body]
            (if (empty? body)
@@ -28,27 +48,76 @@
                         (next body)))))))))
 
 (def app-frame (JFrame. "File Manager"))
+(def current-folder (atom ""))
+(def selected-photo (atom ""))
+(def folder-root (atom ""))
 
-(defn init-app []
-  (let [panel (doto (JPanel. (GridBagLayout.))
-                (grid-bag-layout
-                    :fill :BOTH
-                    :gridx 0, :gridy 0
-                    (JButton. "Button One")
-                    :gridx 1, :gridy 0
-                    (JButton. "Button Two")
-                    :gridx 2, :gridy 0
-                    (JButton. "Button Three")
-                    :ipady 40
-                    :gridwidth 3, :gridheight 2
-                    :gridx 0, :gridy 1
-                    (JButton. "Long-named Button 4")
-                    :ipady 0
-                    :fill :HORIZONTAL, :insets (Insets. 10 0 0 0), :gridwidth 2
-                    :gridx 1 :gridy 4
-                    (JButton. "Button Five")))]
+
+;; Creates App Panes:
+;; +---+-------------+---+
+;; | A | B           | C |
+;; |   |             |   |
+;; |   |             +---+
+;; |   |             | D |
+;; |   |             |   |
+;; +---+-------------+---+
+;;
+;; A - Tree Frame (Shows tree listing of files/directories, starting from a specified root)
+;; B - File Explorer (Shows Windows-Explorer style listing of files/folders in grid)
+;; C - Info Pane (Shows details of the selected file, i.e. EXIF data)
+;; D - GPS Pane (Shows GPS info of current file and map of its location)
+
+;; TODO: Link panes to functions and begin populating them
+
+(defn tree-frame
+  "Creates tree-style listing of files from root"
+  []
+  (doto (JScrollPane. (doto (JTree.)
+                        (.setBackground Color/GREEN)
+                        (.setPreferredSize (Dimension. 300 300))))))
+    
+
+(defn file-explorer
+  "Creates grid layount of files in currently open directory"
+  []
+  (doto (JScrollPane. (doto (JPanel.)
+                        (.setBackground Color/RED)
+                        (.setPreferredSize (Dimension. 300 300))))))
+
+(defn info-pane
+  "Displays info and metadata from the selected file"
+  []
+  (doto (JScrollPane. (doto (JPanel.)
+                        (.setBackground Color/BLUE)
+                        (.setPreferredSize (Dimension. 300 300))))))
+
+(defn gps-pane
+  "Creates pane with GPS Location"
+  []
+  (doto (JScrollPane. (doto (JPanel.)
+                        (.setBackground Color/ORANGE)
+                        (.setPreferredSize (Dimension. 300 300))))))
+
+(defn init-app
+  "Initializes frame and creates panes corresponding to the user interface"
+  []
+  (let [content-pane (doto (JPanel. (GridBagLayout.))
+                       (grid-bag-layout
+                           :fill :BOTH :weightx 1 :weighty 1
+                           :gridx 0 :gridy 0 :gridheight 2
+                           (tree-frame)
+                           :gridx 1 :gridy 0 :gridheight 2
+                           (file-explorer)
+                           :gridx 2 :gridy 0 :gridheight 1
+                           (info-pane)
+                           :gridx 2 :gridy 1 :gridheight 1
+                           (gps-pane))
+                       (.setSize 500 500))]
     (doto app-frame
       (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-      (.setContentPane panel)
+      (.setLayout (BorderLayout.))
+      (.add content-pane)
+      (.setLocationRelativeTo nil)
       (.pack)
       (.setVisible true))))
+
