ab076629b43fa9767170222ec3f4ef00a05726dc
Jim Chen <theyee0@gmail.com>
2025-07-08 18:45:49 -0700

    Added metadata-extractor and jxmapviewer as dependencies
    metadata-extractor: Extracts EXIF data and image properties given
    filepath; useful for showing image properties and extracting GPS Data
    
    jxmapviewer: Uses OpenStreetMap to generate a configurable Map; useful
    for showing markers on a map representing GPS Data from images in a path

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/java.jdbc "0.7.8"]
-                 [org.xerial/sqlite-jdbc "3.23.1"]]
+                 [org.xerial/sqlite-jdbc "3.23.1"]
+                 [com.drewnoakes/metadata-extractor "2.19.0"]
+                 [org.jxmapviewer/jxmapviewer2 "2.8"]]
   :aot [file-manager.filetree-model]
   :main ^:skip-aot file-manager.core
   :target-path "target/%s"
