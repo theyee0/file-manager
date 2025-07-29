56bbb32de9769b241a6c1f698028209a9896b15d
Jim Chen <theyee0@gmail.com>
2025-07-28 20:46:23 -0700

    Edited default entry point
    3 enhancements:
    1. Connects to interface.clj to initialize the app
    2. Uses Flatlaf for a modern light theme
    3. Initializes the SQL Database used to store data

--- src/file_manager/core.clj
+++ src/file_manager/core.clj
@@ -2,12 +2,17 @@
   (:gen-class)
   (:use file-manager.file-utils)
   (:use file-manager.tags)
+  (:use file-manager.interface)
   (:import [javax.swing JFrame JLabel JButton]
-           [java.awt.event WindowListener])
+           [java.awt.event WindowListener]
+           [com.formdev.flatlaf FlatLaf FlatLightLaf])
   (:require [clojure.java.io :as io])
   (:require clojure.pprint))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (initialize-db))
+  (do
+    (initialize-db)
+    (. FlatLightLaf setup)
+    (init-app)))
